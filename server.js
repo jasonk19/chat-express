180b3d9ee07f8b59d662a17e983591e63036f76d
jask-19 <16520214@std.stei.itb.ac.id>
2021-08-14 23:47:39 +0700

    set static to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const express = require('express');
 
 const app = express();
+
+// set static
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 3000 || process.env.PORT;
 
