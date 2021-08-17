822179cc4c84dddc89d2b846736786a4cf952f0e
jask-19 <16520214@std.stei.itb.ac.id>
2021-08-17 17:14:32 +0700

    socket initialization

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const path = require('path')
 const express = require('express');
+const http = require('http')
+const socketio = require('socket.io')
+
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
 
 // set static
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Run when client connects
+io.on('connection', socket => {
+  console.log('New WS connection...')
+
+  socket.emit('message', 'Welcome to ChatCord!')
+})
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Serving running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Serving running on port ${PORT}`))
